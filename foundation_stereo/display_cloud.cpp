3ae7d2c04f07048399955eca64fd4a3be0d68f2b
kaylorchen <kaylor.chen@qq.com>
2025-11-27 21:59:52 +0800

    修改显示参数

--- foundation_stereo/display_cloud.cpp
+++ foundation_stereo/display_cloud.cpp
@@ -7,9 +7,15 @@
 
 DisplayCloud::DisplayCloud() {
   viewer_ = std::make_shared<pcl::visualization::PCLVisualizer>("3D Viewer");
-  viewer_->setBackgroundColor(0, 0, 0);
+  viewer_->setBackgroundColor(255, 255, 255);
   viewer_->addCoordinateSystem(1.0);
   viewer_->initCameraParameters();
+  // 设置相机位置，看向Z轴方向，扩大视野
+  viewer_->setCameraPosition(0, 0, -2.0, // 相机位置 (在原点上方)
+                             0, 0, 1,  // 观察点位置 (看向Z轴正方向)
+                             0, -1, 0, // 相机上方向量
+                             0.0       // 视口大小 (0.0表示自动)
+  );
   KAYLORDUT_LOG_INFO("3D Viewer initialized");
 }
 void DisplayCloud::ShowCloud(
